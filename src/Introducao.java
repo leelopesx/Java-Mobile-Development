7f6c5cdc88a6cde9d773d5fb2dae12bb388bc1b3
lee.lopesz <lb.lopes04@gmail.com>
2026-03-11 22:36:42 -0300

    11/03/2026

--- src/Introducao.java
+++ src/Introducao.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
+
 public class Introducao {
     public static void main(String[] args) {
         // Print em Java
         System.out.println("Olá mundo!");
 
-        // Variáveis  do tipo real
+        // Variáveis  do tipo real (Consegue armazenar números maiores do que o float)
         double x,y,z,w;
 
         // Variável do tipo inteiro
@@ -27,5 +29,45 @@ public class Introducao {
         // Utilizando apenas duas casas decimais
         System.out.printf("%s%.2f\n", "O valor com 10% de desconto será  " , desconto10);
 
+        // Classe Scanner - ler uma informação inserida pelo o usuário (leitura de dados) (famoso input)
+        Scanner le = new Scanner(System.in); // System.in - ler dados digitados pelo usuário
+        int qtd;
+        double valor;
+
+        // Ler as variáveis de acordo com o seu tipo
+        System.out.print("Informe a quantidade:" );
+        qtd = le.nextInt(); // Ler o valor como um número inteiro
+        System.out.print("Informe o valor:" );
+        valor = le.nextDouble();
+
+        double media = valor/qtd;
+        System.out.println("Média do preço de cada produto: " + media);
+
+        // Exercicio 02
+        double F, C;
+        System.out.print("Informe a temperatura em Farenheit:" );
+        F = le.nextDouble();
+        C = 5/9.0 * (F-32);
+        System.out.printf("%s%.2f\n" , "A temperatura convertida para graus Celcius é de: " , C , "C");
+
+
+        // Condicional
+        int n1, n2;
+        System.out.print("N1 = ");
+        n1 = le.nextInt();
+        System.out.print("N2 = ");
+        n2 = le.nextInt();
+
+        if (n1 == n2){
+            System.out.print("Números iguais");
+        } else if (n1 > n2) {
+            System.out.print("N1 é maior que N2");
+        } else {
+            System.out.print("N2 é maior que N1");
+        }
+
+        le.close(); // Finalização do Scanner (para desocupar na memória)
+
+        // Exercicio 03 (condicional)
     }
 }
